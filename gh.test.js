43e14c425ecacba9c9b1165ac6a5c4a92d2290fd
Yar neto <quatro30@gmail.com>
2025-08-07 16:46:03 +0700

    modified:   gh.test.js

--- gh.test.js
+++ gh.test.js
@@ -1,49 +1,39 @@
 let page;
-jest.setTimeout(25000);
 
 beforeEach(async () => {
   page = await browser.newPage();
-  await page.goto("https://github.com/team");
+  await page.goto("https://github.com/team", { waitUntil: 'load' });
 });
 
 afterEach(async () => {
   if (page) await page.close();
-  await page.waitForTimeout(2000);
 });
 
 describe("Github page tests", () => {
   test("The h1 header content'", async () => {
-
-    await page.waitForTimeout(2000);
     const firstLink = await page.$("header div div a");
     await firstLink.click();
     await page.waitForSelector('h1');
     const title2 = await page.title();
     expect(title2).toEqual('GitHub · Build and ship software on a single, collaborative platform · GitHub');
-  });
+  }, 6000);
 
   test("The first link attribute", async () => {
-
-    await page.waitForTimeout(200);
     const actual = await page.$eval("a", link => link.getAttribute('href') );
     expect(actual).toEqual("#start-of-content");
-  });
+  }, 5000);
 
   test("The page contains Sign in button", async () => {
-
-    await page.waitForTimeout(200);
     const btnSelector = ".btn-large-mktg.btn-mktg";
     await page.waitForSelector(btnSelector, {
       visible: true,
     });
     const actual = await page.$eval(btnSelector, link => link.textContent);
     expect(actual).toContain("Get started with Team")
-  });
+  }, 5000);
 });
 
   test("The Actions link attribute", async () => {
-  await page.waitForTimeout(200);
-
   const selector1 = "li:nth-child(1) button:nth-child(1)";
   await page.waitForSelector(selector1);
   await page.click(selector1);
@@ -55,35 +45,25 @@ describe("Github page tests", () => {
   await page.waitForSelector('#hero-section-brand-heading', { visible: true });
   const actual = await page.$eval('#hero-section-brand-heading', el => el.textContent.trim());
   expect(actual).toBe("Automate your workflow from idea to production");
-});
+}, 5000);
 
   test("The Solutions DevOps title", async () => {
-  await page.waitForTimeout(200);
-  await page.hover('li:nth-child(2) button:nth-child(1)');
+  await page.hover('li:nth-child(2) button:nth-child(1)'), { delay: 200 };
   await page.waitForSelector('a[href="/solutions/use-case/devops"]', { visible: true }); 
-  await page.waitForTimeout(200);
   await page.click('a[href="/solutions/use-case/devops"]');
-  await page.waitForTimeout(2000);
   
   await page.waitForSelector('#hero-section-brand-heading', { visible: true });
   const actual = await page.$eval('#hero-section-brand-heading', el => el.textContent.trim());
   expect(actual).toBe("The unified platform for your DevOps lifecycle");
-});
+}, 5000);
 
-test("The Partners page title", async () => {
-  await page.waitForTimeout(200);
-  await page.hover('li:nth-child(3) button:nth-child(1)');
-  await page.waitForTimeout(200);
-  const [partnerLink] = await page.$x('//li[contains(@class, "HeaderMenu-item")]//a[@href="https://partner.github.com"]');
-  const [newPagePromise] = await Promise.all([
-    new Promise(resolve => browser.once('targetcreated', target => resolve(target.page()))),
-    partnerLink.click()
-  ]);
-  const newPage = await newPagePromise;
+test("The Pricing title", async () => {
+  await page.click('a.HeaderMenu-link.no-underline.px-0.px-lg-2.py-3.py-lg-2.d-block.d-lg-inline-block', { delay: 2000 });
+  await page.waitForSelector('h1.h2-mktg');
+  const actualHeaderText = await page.$eval('h1.h2-mktg', el => el.textContent.trim());
 
-  await newPage.waitForSelector('h1.h0-mktg.text-bold.mb-3', { visible: true });
-  const actual = await newPage.$eval('h1.h0-mktg.text-bold.mb-3', el => el.textContent.trim());
-  expect(actual).toBe("Why partner with GitHub?");
-});
+  expect(actualHeaderText).toBe('Try the Copilot-powered platform');
+}, 5000);
+
 
 
